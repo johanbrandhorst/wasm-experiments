8725450750b2cb8019553c2d665ee862d4c5c8cf
Johan Brandhorst <johan.brandhorst@gmail.com>
2018-05-13 15:59:25 +0100

    Move Fetch Transport to separate repo

--- fetch/main.go
+++ fetch/main.go
@@ -4,14 +4,26 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
-	"github.com/johanbrandhorst/wasm-experiments/fetch/fetch"
+	"github.com/johanbrandhorst/fetch"
 )
 
 func main() {
 	c := http.Client{
 		Transport: &fetch.Transport{},
 	}
-	fmt.Println(c.Get("https://api.github.com"))
+	resp, err := c.Get("https://api.github.com")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(b))
 }
