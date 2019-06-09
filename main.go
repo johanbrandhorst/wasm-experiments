b8bf9995ca90ec234231879db7142d3039b8a592
Johan Brandhorst <johan.brandhorst@gmail.com>
2019-06-09 19:00:06 +0100

    Log messages when server exits

--- main.go
+++ main.go
@@ -8,11 +8,14 @@ import (
 func main() {
 	fs := http.FileServer(http.Dir("./html"))
 	log.Print("Serving on http://localhost:8080")
-	http.ListenAndServe(":8080", http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+	err := http.ListenAndServe(":8080", http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
 		if req.URL.Path == "/test.wasm" {
 			resp.Header().Set("content-type", "application/wasm")
 		}
 
 		fs.ServeHTTP(resp, req)
 	}))
+	if err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
 }
